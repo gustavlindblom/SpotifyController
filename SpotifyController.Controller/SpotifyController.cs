2d35aac132b28a70709e9046693b6914ac38d0eb
Gustav Lindblom <gustav.lindblom@pentia.se>
2018-02-09 16:53:46 +0100

    Implemented wrappers around SpotifyAPI Event args.
    Implemented event handlers around SpotifyAPI events.

--- SpotifyController.Controller/SpotifyController.cs
+++ SpotifyController.Controller/SpotifyController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
@@ -18,6 +19,10 @@ namespace SpotifyController.Controller
 {
     public class SpotifyController
     {
+        public event EventHandler<PlayStateEventArgs> PlayStateChanged;
+        public event EventHandler<TrackChangeEventArgs> TrackChanged;
+        public event EventHandler<VolumeChangeEventArgs> VolumeChanged;
+        public event EventHandler<TrackTimeChangeEventArgs> TrackTimeChanged;
 
         private readonly SpotifyLocalAPI _client;
         private SpotifyWebAPI _webApi;
@@ -26,9 +31,13 @@ namespace SpotifyController.Controller
         private readonly string _clientSecret;
         private readonly string _accessToken;
 
+        private readonly string _testId = "06qjd7U01Ma8ZtY8B30Lim";
+
         public SpotifyController(string clientId, string clientSecret, string accessToken = null)
         {
             _client = new SpotifyLocalAPI();
+
+            
 
             _clientId = clientId;
             _clientSecret = clientSecret;
@@ -70,11 +79,22 @@ namespace SpotifyController.Controller
                 UseAuth = true,
                 UseAutoRetry = true
             };
+
+            if (_webApi.GetTrack(_testId).HasError())
+                GetUserPermissions();
         }
 
         public bool Connect()
         {
-            return _client.Connect();
+            if (!_client.Connect())
+                return false;
+
+            _client.ListenForEvents = true;
+            _client.OnPlayStateChange += ClientOnPlayStateChange;
+            _client.OnTrackChange += ClientOnTrackChange;
+            _client.OnTrackTimeChange += ClientOnTrackTimeChange;
+            _client.OnVolumeChange += ClientOnVolumeChange;
+            return true;
         }
 
         public TrackModel CurrentTrack()
@@ -103,5 +123,93 @@ namespace SpotifyController.Controller
         {
             _client.Dispose();
         }
+
+        #region EVENT_HANDLERS
+        private void ClientOnVolumeChange(object sender, SpotifyAPI.Local.VolumeChangeEventArgs volumeChangeEventArgs)
+        {
+            VolumeChanged?.Invoke(this, (VolumeChangeEventArgs)volumeChangeEventArgs);
+        }
+
+        private void ClientOnTrackTimeChange(object sender, SpotifyAPI.Local.TrackTimeChangeEventArgs trackTimeChangeEventArgs)
+        {
+            TrackTimeChanged?.Invoke(this, (TrackTimeChangeEventArgs)trackTimeChangeEventArgs);
+        }
+
+        private void ClientOnTrackChange(object sender, SpotifyAPI.Local.TrackChangeEventArgs trackChangeEventArgs)
+        {
+            TrackChanged?.Invoke(this, (TrackChangeEventArgs)trackChangeEventArgs);
+        }
+
+        private void ClientOnPlayStateChange(object sender, SpotifyAPI.Local.PlayStateEventArgs playStateEventArgs)
+        {
+            PlayStateChanged?.Invoke(this, (PlayStateEventArgs)playStateEventArgs);
+        }
+        #endregion
+
+        #region EVENT_ARGS_WRAPPERS
+
+        public class PlayStateEventArgs : EventArgs
+        {
+            public bool Playing { get; set; }
+
+            public static explicit operator PlayStateEventArgs(SpotifyAPI.Local.PlayStateEventArgs args)
+            {
+                return new PlayStateEventArgs() {Playing = args.Playing};
+            }
+        }
+
+        public class TrackChangeEventArgs : EventArgs
+        {
+
+            public TrackModel PreviousTrack { get; set; }
+            public TrackModel CurrentTrack { get; set; }
+
+            public static explicit operator TrackChangeEventArgs(SpotifyAPI.Local.TrackChangeEventArgs args)
+            {
+                return new TrackChangeEventArgs()
+                {
+                    PreviousTrack = new TrackModel()
+                    {
+                        Length = args.OldTrack.Length,
+                        Title = args.OldTrack.TrackResource.Name,
+                        Uri = args.OldTrack.TrackResource.Uri
+                    },
+                    CurrentTrack = new TrackModel()
+                    {
+                        Length = args.NewTrack.Length,
+                        Title = args.NewTrack.TrackResource.Name,
+                        Uri = args.NewTrack.TrackResource.Uri
+                    }
+                };
+            }
+        }
+
+        public class TrackTimeChangeEventArgs : EventArgs
+        {
+            public double TrackTime { get; set; }
+
+            public static explicit operator TrackTimeChangeEventArgs(SpotifyAPI.Local.TrackTimeChangeEventArgs args)
+            {
+                return new TrackTimeChangeEventArgs() {TrackTime = args.TrackTime};
+            }
+        }
+
+        public class VolumeChangeEventArgs : EventArgs
+        {
+
+            public double PreviousVolume { get; set; }
+            public double CurrentVolume { get; set; }
+
+            public static explicit operator VolumeChangeEventArgs(SpotifyAPI.Local.VolumeChangeEventArgs args)
+            {
+                return new VolumeChangeEventArgs()
+                {
+                    CurrentVolume = args.NewVolume,
+                    PreviousVolume = args.OldVolume
+                };
+            }
+        }
+
+        #endregion
     }
 }
--- SpotifyController.TestApplication/Program.cs
+++ SpotifyController.TestApplication/Program.cs
@@ -25,11 +25,34 @@ namespace SpotifyController.TestApplication
 
             var album = controller.CurrentAlbum();
 
+            controller.PlayStateChanged += Controller_PlayStateChanged;
+            controller.TrackChanged += Controller_TrackChanged;
+            controller.VolumeChanged += Controller_VolumeChanged;
+            controller.TrackTimeChanged += Controller_TrackTimeChanged;
+
             Console.WriteLine($"Current album: {album.Title}\n" +
                               $"Release date: {album.ReleaseDate:yyyy-MM-dd}\n" +
                               $"ID: {album.SpotifyId}");
 
             Console.ReadKey();
+        }
+
+        private static void Controller_TrackTimeChanged(object sender, Controller.SpotifyController.TrackTimeChangeEventArgs e)
+        {
+        }
+
+        private static void Controller_VolumeChanged(object sender, Controller.SpotifyController.VolumeChangeEventArgs e)
+        {
+        }
+
+        private static void Controller_TrackChanged(object sender, Controller.SpotifyController.TrackChangeEventArgs e)
+        {
+            Console.Clear();
+            Console.WriteLine($"Now playing: {e.CurrentTrack.Title}");
+        }
+
+        private static void Controller_PlayStateChanged(object sender, Controller.SpotifyController.PlayStateEventArgs e)
+        {
         }
     }
 }
